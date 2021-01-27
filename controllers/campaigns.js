dbf0818d7f80f8e3d485411a9bca1e32ec610645
Michael Watts <wattsjmichael@gmail.com>
2021-01-27 14:06:49 -0800

    Adds Get and Get by IID logic

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -2,33 +2,46 @@ const Campaign = require('../models/Campaign')
 // Get All Campaigns
 // GET /api/v1/campaigns
 // Public
-exports.getCampaigns = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Show all campaigns'
-    });
+exports.getCampaigns = async (req, res, next) => {
+  try {
+    const campaigns = await Campaign.find();
+    res.status(200).json({ success: true, data: campaigns });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  };
 }
 
 // Get single Campaign
 // GET /api/v1/campaigns/:id
 // Public
-exports.getCampaign = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Display Campaign ${req.params.id}`
-    });
-}
+exports.getCampaign = async (req, res, next) => {
+  try {
+    const campaign = await Campaign.findById(req.params.id);
+
+    if (!campaign) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: campaign })
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 // Create single Campaign
 // POST /api/v1/campaigns
 // Private
-exports.createCampaign = (req, res, next) => {
-  console.log(req.body)
-  res.status(200).json({
-    success: true,
-    msg: 'Create new campaign',
-    body: req.body
+exports.createCampaign = async (req, res, next) => {
+  try {
+    const campaign = await Campaign.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: campaign
     });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 }
 
 // Update single Campaign
@@ -38,7 +51,7 @@ exports.updateCampaign = (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: `Update Campaign ${req.params.id}`
-    });
+  });
 }
 
 // Delete single Campaign
@@ -48,5 +61,5 @@ exports.deleteCampaign = (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: `Delete Campaign ${req.params.id}`
-    });
+  });
 }
